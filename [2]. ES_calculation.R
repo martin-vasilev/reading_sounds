6f1995829b9b709693135ef03d7bdaa565d9df69
martin-vasilev <mvasilev@bournemouth.ac.uk>
2016-09-01 10:46:40 +0100

    more code

--- [2]. ES_calculation.R
+++ [2]. ES_calculation.R
@@ -16,6 +16,9 @@ load("Data/data.Rda")
 # Calculate Effect sizes
 es<- get_es(data)
 
+colnames(es)<- c("ID", "cit", "year", "design", "sound", "sound_type", "db",
+                 "task", "measure", "d", "var_d", "g", "var_d")
+
 
 # Save data: 
 save(es, file= "Data/es.Rda")
