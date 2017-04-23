79ddcb3743c2c825cefedf9f3b7784b09c93996c
martin-vasilev <mvasilev@bournemouth.ac.uk>
2017-04-23 17:28:05 +0100

    more to pub bias plot

--- Pub_bias/visual_investigation.R
+++ Pub_bias/visual_investigation.R
@@ -15,13 +15,13 @@ res_music<- rma(g, g_var, data=subset(data, sound=="music"), method="REML")
 res_noise<- rma(g, g_var, data=subset(data, sound=="noise"), method="REML")
 res_speech<- rma(g, g_var, data=subset(data, sound=="speech"), method="REML")
 
-funnel(res_music, yaxis="seinv", main="Inverse Standard Error")
-funnel(res_noise, yaxis="seinv", main="Inverse Standard Error")
-funnel(res_speech, yaxis="seinv", main="Inverse Standard Error")
+#funnel(res_music, yaxis="seinv", main="Inverse Standard Error")
+#funnel(res_noise, yaxis="seinv", main="Inverse Standard Error")
+#funnel(res_speech, yaxis="seinv", main="Inverse Standard Error")
 
 
 png('Pub_bias/panel_plot.png', width = 2400, height = 3200, units = "px", res=300, type="cairo")
-par(mfrow=c(3,2), mar= c(5,4.5,5,1))
+par(mfrow=c(3,2), mar= c(5,5,5,1))
 
 # funnel plots:
 res <- rma(g, g_var, data=subset(data, is.element(task, RC) & measure!="reading_speed"), method="REML")
@@ -29,6 +29,36 @@ funnel(res, yaxis="sei", main="a", family="serif", cex.lab=1.7, cex.axis=1.5, ce
        at= c(-2,-1,0,1), xlab= "Effect size (g)")
 funnel(res, yaxis="seinv", main="b", family="serif", cex.lab=1.7, cex.axis=1.5, cex.main=2.5, digits=2,
        at= c(-2,-1,0,1), xlab= "Effect size (g)")
+
+
+## Trim and fill:
+taf<- trimfill(res)
+# No missing studies estimated!
+
+
+## Radial plot: 
+radial(res, main= "c", family="serif", cex.lab=1.7, cex.axis=1.5, cex.main=2.5, xlab= "Precision (1/SE)",
+      zlab = "Effect size (g)/ SE", center = F, cex = 1.30, pch = 16, aty = c(-1.5,-1, -0.5, 0, 0.5), 
+      level = 95)
+
+
+## Sample size by effect size (+ breakdown by design)
+d1<- subset(data, is.element(task, RC) & measure!="reading_speed")
+d1$N_all<- NULL
+for(i in 1:nrow(d1)){
+  if(d1$design[i]=="between"){
+    d1$N_all[i]<- d1$N_C[i]+ d1$N_E[i] 
+  }else{
+    d1$N_all[i]<- d1$N_C[i]
+  }
+}
+
+plot(d1$g[data$design=="between"], d1$N_all[data$design=="between"], family="serif",
+     cex.lab=1.7, cex.axis=1.5, cex.main=2.5, main="d",
+     xlab= "Effect size (g)", ylab= "Sample size", pch=21, bg=pallete[3], cex=1.5, ylim= c(0, 340))
+points(d1$g[data$design=="within"], d1$N_all[data$design=="within"], pch=21, bg=pallete[2], cex=1.5)
+legend(-0.155, 340, c('between-subject', 'within-subject'),pch = c(21, 21), pt.bg = c(pallete[3], pallete[2]), cex=1.2, 
+       title="Design")
 
 
 # effect sizes by publication date:
@@ -55,5 +85,8 @@ bubble(x = mr2, family="serif", cex.lab=1.7, cex.axis=1.5, cex.main=2.5, xlab= "
 abline(mr2$b, col="red", lwd=2, lt=5)
 axis(1, at = seq(0.5, 5.5, by = 1), srt=90, family= "serif", cex.axis = 1.5)
 axis(2, at = seq(-2, 1, by = 0.5), srt=90, family= "serif", cex.axis = 1.5)
+
+
+#cm<- metacum(x = m1, pooled = "random", year)
 
 dev.off()
